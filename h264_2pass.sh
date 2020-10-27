49c204de38f159a6439dca4bf37a30d288da6760
Ray Xu <ruijxu@microsoft.com>
2020-10-26 19:05:13 -0700

    small opt

--- h264_2pass.sh
+++ h264_2pass.sh
@@ -1,2 +1,2 @@
-ffmpeg -y -i 'src_video/MzQpIDYgUHJpbmNpcGxlczogTm8gU2Vla2luZyAtIENoYW4gUWkgLSAwMS8yOC8yMDE4Cg==.mp4' -vf scale=640x480 -c:v libx264 -b:v 512k -g 240 -threads 20 -preset veryslow -crf 34 -pass 1 -an -f null /dev/null && \
-ffmpeg -y -i 'src_video/MzQpIDYgUHJpbmNpcGxlczogTm8gU2Vla2luZyAtIENoYW4gUWkgLSAwMS8yOC8yMDE4Cg==.mp4' -vf scale=640x480 -c:v libx264 -b:v 512k -g 240 -threads 20 -preset veryslow -crf 34 -pass 2 -c:a copy -pix_fmt yuv420p -movflags +faststart 'transcoded_video/MzQpIDYgUHJpbmNpcGxlczogTm8gU2Vla2luZyAtIENoYW4gUWkgLSAwMS8yOC8yMDE4Cg==.mp4'
+ffmpeg -y -i "src_video/${VIDEO_NAME}.mp4" -vf scale=640x480 -c:v libx264 -b:v 512k -g 240 -threads 20 -preset veryslow -crf 34 -pass 1 -an -f null /dev/null && \
+ffmpeg -y -i "src_video/${VIDEO_NAME}.mp4" -vf scale=640x480 -c:v libx264 -b:v 512k -g 240 -threads 20 -preset veryslow -crf 34 -pass 2 -c:a copy -pix_fmt yuv420p -movflags +faststart "transcoded_video/${VIDEO_NAME}.mp4"
