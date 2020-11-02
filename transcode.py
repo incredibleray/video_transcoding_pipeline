8a1a938d72f2f31245d49b6d059e974f1124b0cc
Ray Xu <ruijxu@microsoft.com>
2020-11-01 19:24:51 -0800

    improve transcode.py

--- transcode.py
+++ transcode.py
@@ -8,6 +8,7 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string("source_video", None, "")
 flags.DEFINE_boolean('debug_logs', False, '')
+flags.DEFINE_boolean('remove_youtube_dl_filename_suffix', True, '')
 
 def main(argv):
   if FLAGS.debug_logs:
@@ -17,8 +18,14 @@ def main(argv):
     logging.fatal('no source video')
     return
 
-  source_video=FLAGS.source_video
-  transcoded_video=base64.b64encode(source_video.encode('ascii')).decode('ascii')
+  source_video_name=FLAGS.source_video
+  if FLAGS.remove_youtube_dl_filename_suffix:
+    youtube_dl_filename_suffix_start_index=source_video_name.rfind('-')
+    source_video_name=source_video_name[:youtube_dl_filename_suffix_start_index]
+  
+  logging.debug('source video name is: %s', source_video_name)
+
+  transcoded_video=base64.b64encode(source_video_name.encode('ascii')).decode('ascii')
 
   pass_1_cmd_template='''
   ffmpeg -y -i "src_video/{source_video}.mp4" -vf scale=640x360 -c:v libx264 -b:v 276k -g 1440 -threads 20 -preset veryslow -crf 36 -pass 1 -an -f null /dev/null
@@ -28,10 +35,14 @@ def main(argv):
   ffmpeg -y -i "src_video/{source_video}.mp4" -vf scale=640x360 -c:v libx264 -b:v 276k -g 1440 -threads 20 -preset veryslow -crf 36 -pass 2 -c:a copy -pix_fmt yuv420p -movflags +faststart "transcoded_video/{transcoded_video}.mp4"
   '''
 
+  generate_thumbnail_cmd_template='''
+  ffmpeg -y -i "src_video/{source_video}.mp4"  -ss 00:06:00 -vframes 1 "transcoded_video/{transcoded_video}.png"
+  '''
+
   transcode_success=True
-  for cmd_template in (pass_1_cmd_template, pass_2_cmd_template,):
+  for cmd_template in (pass_1_cmd_template, pass_2_cmd_template, generate_thumbnail_cmd_template,):
     cmd=cmd_template.format(
-      source_video=source_video,
+      source_video=FLAGS.source_video,
       transcoded_video=transcoded_video)
 
     args = shlex.split(cmd)
