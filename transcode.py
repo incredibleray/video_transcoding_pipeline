4bc20bf749f04f8f0076ab7d070fe26a6ece7612
Ray Xu <ruijxu@microsoft.com>
2020-11-04 16:44:26 -0800

    try vp9 lower quality

--- transcode.py
+++ transcode.py
@@ -4,12 +4,14 @@ import shlex
 import subprocess
 import base64
 import re
+import ffmpeg_command
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string("source_video", None, "")
 flags.DEFINE_boolean('debug_logs', False, '')
-flags.DEFINE_boolean('remove_youtube_dl_filename_suffix', True, '')
+flags.DEFINE_boolean('remove_youtube_dl_filename_suffix', False, '')
+flags.DEFINE_boolean('keep_source_video_filename', False, '')
 
 def main(argv):
   if FLAGS.debug_logs:
@@ -29,22 +31,17 @@ def main(argv):
   
   logging.debug('source video name is: %s', source_video_name)
 
-  transcoded_video=base64.b64encode(source_video_name.encode('ascii')).decode('ascii')
-
-  pass_1_cmd_template='''
-  ffmpeg -y -i "src_video/{source_video}.mp4" -vf scale=640x360 -c:v libx264 -b:v 276k -g 1440 -threads 20 -preset veryslow -crf 36 -pass 1 -an -f null /dev/null
-  '''
-
-  pass_2_cmd_template='''
-  ffmpeg -y -i "src_video/{source_video}.mp4" -vf scale=640x360 -c:v libx264 -b:v 276k -g 1440 -threads 20 -preset veryslow -crf 36 -pass 2 -c:a copy -pix_fmt yuv420p -movflags +faststart "transcoded_video/{transcoded_video}.mp4"
-  '''
-
-  generate_thumbnail_cmd_template='''
-  ffmpeg -y -i "src_video/{source_video}.mp4"  -ss 00:06:00 -vframes 1 "transcoded_video/{transcoded_video}.png"
-  '''
+  if FLAGS.keep_source_video_filename:
+    transcoded_video=source_video_name
+  else:
+    transcoded_video=base64.b64encode(source_video_name.encode('ascii')).decode('ascii')
 
   transcode_success=True
-  for cmd_template in (pass_1_cmd_template, pass_2_cmd_template, generate_thumbnail_cmd_template,):
+  for cmd_template in (
+    ffmpeg_command.vp9_360p_low_quality_pass_1_cmd_template,
+    ffmpeg_command.vp9_360p_low_quality_pass_2_cmd_template,
+    # generate_thumbnail_cmd_template,
+    ):
     cmd=cmd_template.format(
       source_video=FLAGS.source_video,
       transcoded_video=transcoded_video)
