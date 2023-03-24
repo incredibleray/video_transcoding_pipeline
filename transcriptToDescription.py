15d80266746e9eb55cf016b0fd6ef9e98e4dffe3
Ray Xu <ruijie.ray.xu@qq.com>
2023-03-24 01:14:11 -0700

    c

--- transcriptToDescription.py
+++ transcriptToDescription.py
@@ -13,53 +13,59 @@ flags.DEFINE_string('transcript', None, '')
 
 
 desc='''00:00 | 🙏 Emitofuo 
-01:00 | Requesting the Dharma
-04:29 | Invocations
-07:34 | Date and Place
-07:44 | Introduction
-07:55 | Slide 45
-12:40 | Slide 46
-14:01 | Slide 47
-16:22 | Slide 48
-18:07 | Anthony Question: Why did Bodhidharma make HuiKe kneel before telling him to wait till snow turns red
-18:31 | Master Answer
-23:16 | Discussion: How does HuiKe survive
-27:05 | Discussion: Sharing the Dharma with others
-31:14 | Did Malcom dye his hair?
-31:47 | Back to Discussion: Sharing Dharma with others
-35:05 | Back to slide 48: Bodhidharma ask his disciple to say something to demonstrate their understanding
-36:51 | Slide 49: DaoFu's answer
-37:36 | Slide 50: ZongChi's answer
-38:00 | Slide 51 DaoYu's answer
-39:34 | Slide 52 HuiKe Bowed
-39:49 | Elaborate on DaoFu's answer
-41:28 | Elaborate on ZongChi's answer
-48:28 | Chinese of DaoYu's answer: 四大本空，五陰非有，而我見處，無一法可得
-54:15 | Elaborate on DaoYu's answer
-1:00:15 | Discussion: Why does DaoYu's answer demonstrate a deeper level of wisdom
-1:09:15 | Word of Caution: Know your limitations. Do not comment on people who have more wisdom than you
- 1:10:01 | Master answers: Why is DaoYu's wisdom higher
-1:13:17 | Elaborate on HuiKe's response
-1:14:59 | How Chan people compliment students: You are my bone marrow
-1:15:39 | Malcom said he did not dye his hair, master calls him liar liar pants on fire
-1:16:34 | Malcom question: what does "You have my bone marrow" mean, you are the same as me?
-1:16:51 | Master answer
-1:19:06 | Jane translates "Liar liar pants on fire" to Chinese
-1:20:47 | Slide 53: 吾本來茲土，傳法救迷情，一花開五葉，結果自然成
-1:24:56 | Slide 54
-1:25:45 | Slide 55
-1:26:14 | Slide 56
-1:26:41 | Diego predicted that master will be seen walking in the Bahamas after he's done
-1:27:16 | Slide 57
-1:27:45 | Slide 58
-1:28:17 | Slide 59: WeiTuo bodhisattva stood in guard when Ven. HuiKe is born
-1:29:10 | Slide 60: parents thereupon named their son ShenGuang-Spritual light
-1:29:47 | Slide 61
-1:31:32 | Slide 62: Death of his parents cause HuiKe to turn to Buddhism
-2:01:58 | Lecture ended on page 90'''
+01:20 | Requesting the Dharma
+10:38 | Introduction
+13:56 | Slide #03
+14:49 | Malcolm's in Korea
+15:34 | Slide #03
+17:50 | Slide #04
+22:44 | Slide #05
+23:13 | Slide #06
+25:46 | Slide #07
+29:07 | Slide #08
+30:27 | Q&A: Did Bodhidharma walk on foot from India to China?
+32:38 | Q&A: Where did Bodhidharma went to in Vietnam? 
+32:59 | Q&A: Did Bodhidharma come to China on a leaf?
+37:38 | Q&A: What is the meaning behind Venerable Bodhidharma leaving on a leaf?
+38:51 | Slide #08 The Sudden Teaching
+40:43 | Slide #09
+43:49 | Slide #10
+44:59 | Slide #11
+49:18 | Q&A: Is it possible that the 6th patriarch is a reincarnation of Shakyamuni Buddha?
+56:17 | Slide #13
+1:00:03 | Slide #14
+1:01:04 | Slide #15
+1:03:46 | Slide #16
+1:05:55 | Anthony: Pronunciation of Nagarjuna.
+1:06:32 | Slide #16-18
+1:08:27 | Slide #19
+1:12:26 | Slide #20
+1:13:24 | Slide #21
+1:16:42 | Slide #22
+1:18:13 | Slide #23
+1:20:41 | Slide #24
+1:23:42 | Slide #25
+1:25:04 | Slide #26
+1:26:20 | Slide #27
+1:26:47 | Slide #28
+1:29:11 | Slide #29
+1:36:20 | Slide #30
+1:37:14 | Malcom: Bird impersonation
+1:38:14 | Slide #31
+1:39:17 | Slide #35
+1:39:36 | Slide #36
+1:43:10 | Slide #40
+1:43:25 | Slide #41
+1:44:52 | Slide #42
+1:48:17 | Discussion: What language does Venerable Bodhidharma and Venerable ShenGuang use to communicate?
+2:06:58 | Slide #43
+2:07:45 | Discussion: How does Venerable Bodhidharma taught Venerable ShenGuang?
+2:14:50 | Slide #43
+2:17:37 | Slide #44
+2:18:25 | Lecture ended on page 44'''
 
 def main(argv):
-    transcript=webvtt.from_sbv(FLAGS.transcript)
+    transcript=webvtt.from_srt(FLAGS.transcript)
 
     segments=[]
 
